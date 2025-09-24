641f343dd76c45b9e2cff4c81fd882e6fa834d69
David Ogunfiditimi Oluwatayo <davidsome2004@gmail.com>
2025-09-24 10:19:40 +0100

    feat: Integrate Redux for state management; add counter slice and configure store

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,23 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { configureStore } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux'
+import counterReducer  from './redux/counterSlice.js'
 // import "../node_modules/bootstrap/dist/css/bootstrap.css"
 // import "../node_modules/bootstrap/dist/js/bootstrap.js"
 
+
+// A reducer is or are a bunch of functions that determines how the state changes in response to actions
+let store = configureStore({
+  reducer: { counterReducer }
+})
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </StrictMode>,
 )
--- src/pages/Herosection.jsx
+++ src/pages/Herosection.jsx
@@ -3,35 +3,39 @@ import myHero from "../assets/header.png"
 import PropsBtn from '../components/PropsBtn'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 const Herosection = () => {
+    let count = useSelector(state => state.counterReducer.count)
+    useSelector(function (state) { return state })
+        console.log(count);
     let navigate = useNavigate()
     useEffect(() => {
         getDashboard()
     }, [])
     let token = localStorage.token
     let url = "http://localhost:3201/user/dashboard"
-    const getDashboard = ()=>{
-        axios.get(url,{
-            headers:{
+    const getDashboard = () => {
+        axios.get(url, {
+            headers: {
                 "Authorization": `Bearer ${token}`,
                 "Content-Type": "application/json",
                 "Accept": "application/json"
             }
         })
-        .then((res)=>{
-            if(!res.data.status){
-                localStorage.removeItem(token)
-                navigate("/signin")
-            }
-            else{
-                console.log(res);
-                
-            }
-        })
-        .catch((err)=>{
-            console.error("Error:",err.response?err.response.data:err);
-        })
+            .then((res) => {
+                if (!res.data.status) {
+                    localStorage.removeItem(token)
+                    navigate("/signin")
+                }
+                else {
+                    // console.log(res);
+
+                }
+            })
+            .catch((err) => {
+                console.error("Error:", err.response ? err.response.data : err);
+            })
     }
     const test = () => {
         alert("Yeah on point")
